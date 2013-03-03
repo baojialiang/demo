1dc5cd6b5599498a609ba257e3de00b973c14415
baojialiang <504824862@qq.com>
2013-03-04 00:24:43 +0800

    create list show create actions

--- app/controllers/vacation_controller.rb
+++ app/controllers/vacation_controller.rb
@@ -1,7 +1,23 @@
 class VacationController < ApplicationController
+  
+  def index
+    list
+    render("list")
+  end
+  
   def list
     @vacations = Vacation.all
     #render("vacation/list")
-    @a = "you tell me!"
+
   end
+  
+  def show
+    @vacation = Vacation.find(params[:id]) 
+    
+  end
+  
+  def new
+    @vacation = Vacation.new
+  end
+  
 end
--- app/models/vacation.rb
+++ app/models/vacation.rb
@@ -2,7 +2,7 @@ class Vacation < ActiveRecord::Base
   # attr_accessible :title, :body
   belongs_to :user
   
-  attr_accessible :leave_text, :remain_hours, :leave_hours
+  attr_accessible :leave_text, :remain_hours, :request_hours
   
   # v = Vacation.new(:leave_text => "hello", :remain_hours => 20, :leave_hours => 10)
   
