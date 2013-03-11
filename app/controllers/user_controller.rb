11b6f44af7b4077e35779cbeb29021434ba5e41a
baojialiang <504824862@qq.com>
2013-03-12 00:11:39 +0800

    redirect user to menu after user register

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -11,14 +11,16 @@ class UserController < ApplicationController
   end
   
   def create
-    @user = User.new(params[:user])
-    @user.vacation = Vacation.new
+    user = User.new(params[:user])
+    user.vacation = Vacation.new
     
-    if @user.save
-      flash[:notice_info] = "user successfully registered, login please"
-      redirect_to(:controller => "access", :action => "login")
+    if user.save
+      flash[:notice_info] = "Register success, welcome! " + user.first_name 
+      session[:user_id] = user.id
+      session[:username] = user.first_name + " " + user.last_name
+      redirect_to(:controller => "access", :action => "menu")
     else
-      display_error_message @user
+      display_error_message user
       render("new")
     end
   end
